b9881e59839e2b4402a91449df15cb83b765b5d8
Ashpete <109915797+Ashpete@users.noreply.github.com>
2022-12-14 04:52:30 +0000

    hello

--- config/index.js
+++ config/index.js
@@ -1,6 +1,3 @@
 module.exports = {
-  username: process.env.TEXTNOW_USERNAME || "anchernithya@gmail.com",
-  password: process.env.TEXTNOW_PASSWORD || "anushka@786",
-  recipient: process.env.TEXTNOW_RECIPIENT || "(929) 377-6589",
-  message: process.env.TEXTNOW_MESSAGE || "good morning message",
-};
+
+  
